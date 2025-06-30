212127d2e66510c9952929a63d33c436307a2cc5
Myung Choi <myungchoi@gmail.com>
2025-06-30 18:31:56 -0400

    BP missing diastolic fixed

--- registry-fhir-server/src/main/java/edu/gatech/chai/omoponfhir/r4/security/MyCapabilityStatement.java
+++ registry-fhir-server/src/main/java/edu/gatech/chai/omoponfhir/r4/security/MyCapabilityStatement.java
@@ -106,8 +106,8 @@ public class MyCapabilityStatement {
 		cs
          .getSoftware()
 		 .setName("Registry OMOPonFHIR")
-         .setVersion("v1.7.4")
-         .setReleaseDateElement(new DateTimeType("2025-02-24"));
+         .setVersion("v1.7.5")
+         .setReleaseDateElement(new DateTimeType("2025-06-30"));
 
 		cs.setPublisher("Georgia Tech Research Institute - HEAT");
 
